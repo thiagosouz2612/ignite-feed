d1b7cc09aea3eb02bd7bf49b4643c9c24ca35e30
Thiago de Souza <thiago.souz2612@gmail.com>
2024-06-11 17:19:14 -0300

    Iniciando projeto Ignite Feed

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    port: 3000,
+  },
+  // for dev
+  server: {
+    port: 3000,
+  },
+})
